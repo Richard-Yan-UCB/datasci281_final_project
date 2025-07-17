58e9a55becb4fe063bb83ba64f222a95705c34dd
Jacqueline Tiffany Yeung <jtyeung168@berkeley.edu>
2025-07-17 09:19:43 -0700

    rerun standardization

--- utils.py
+++ utils.py
@@ -1,15 +1,29 @@
-from scipy import signal
 import numpy as np
+from skimage import color
+from skimage.transform import rescale
+import cv2 as cv
 
-""" create a 2-D gaussian blurr filter for a given mean and std """
-def create_2d_gaussian(size=9, std=1.5):
-    gaussian_1d = signal.windows.gaussian(size,std=std)
-    gaussian_2d = np.outer(gaussian_1d, gaussian_1d)
-    gaussian_2d = gaussian_2d/(gaussian_2d.sum())
-    return gaussian_2d
-
-
-""" normalize teh image between 0 and 1 """
+""" normalize the image between 0 and 1 """
 def normalize_img(img):
     normalized = (img - img.min())/(img.max() - img.min())    
     return normalized
+
+""" 
+convert to grayscale
+normalize the image between 0 and 1
+resize image to im_size 
+"""
+def preprocess_image(img_path, im_size=512):
+    img = cv.imread(img_path)
+    img_grayscale = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    normalized = normalize_img(img_grayscale)
+    rescaled = rescale_img(img=normalized, standard=im_size)
+    return rescaled
+
+def rescale_img(img, standard=256):
+  # rescale short side to standard size, then crop center
+  scale = standard / min(img.shape[:2])
+  img = rescale(img, scale, anti_aliasing=True)
+  img = img[int(img.shape[0]/2 - standard/2) : int(img.shape[0]/2 + standard/2),
+            int(img.shape[1]/2 - standard/2) : int(img.shape[1]/2 + standard/2)]
+  return img
