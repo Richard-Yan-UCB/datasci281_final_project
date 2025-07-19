d94cb3f5906dee48d27c0c4082abf791f7aa4435
richardyan <rjyan1@asu.edu>
2025-07-19 11:54:29 -0700

    Generating new folds with rescale

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from scipy import signal
 import numpy as np
 import cv2 as cv
+from skimage.transform import rescale
 
 """ create a 2-D gaussian blurr filter for a given mean and std """
 def create_2d_gaussian(size=9, std=1.5):
@@ -21,12 +22,21 @@ convert to grayscale
 normalize teh image between 0 and 1
 resize image to im_size """
 
-def preprocess_image(img_path, im_size = (512,512)):
+def preprocess_image(img_path, im_size=512):
     img = cv.imread(img_path)
     img_grayscale = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    normalized = (img_grayscale - img_grayscale.min())/(img_grayscale.max() - img_grayscale.min())*255
-    resize = cv.resize(normalized, im_size)
-    return resize
+    normalized = normalize_img(img_grayscale)
+    rescaled = rescale_img(img=normalized, standard=im_size)
+    return rescaled
+
+def rescale_img(img, standard=256):
+  # rescale short side to standard size, then crop center
+  print("rescaled")
+  scale = standard / min(img.shape[:2])
+  img = rescale(img, scale, anti_aliasing=True)
+  img = img[int(img.shape[0]/2 - standard/2) : int(img.shape[0]/2 + standard/2),
+            int(img.shape[1]/2 - standard/2) : int(img.shape[1]/2 + standard/2)]
+  return img
 
 
 
